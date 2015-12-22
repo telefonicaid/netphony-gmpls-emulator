c927852163875f9f6a6f235418d013284cd7cca0
Ruben Rosales <e.gcto.tid@telefonica.com>
2015-12-22 15:23:39 +0100

    Changes on EmulatedPCCPCEPSession and NoIPNMSDispatcher

--- src/es/tid/netManager/uni/NoIPNMSDispatcher.java
+++ src/es/tid/netManager/uni/NoIPNMSDispatcher.java
@@ -8,11 +8,6 @@ import java.net.UnknownHostException;
 import es.tid.ipnms.datamodel.misc.AuthInfo;
 import es.tid.ipnms.datamodel.router.RouterDesc;
 import es.tid.rsvp.objects.subobjects.UnnumberIfIDEROSubobject;
-<<<<<<< HEAD
-=======
-import es.tid.ipnms.datamodel.misc.AuthInfo;
-import es.tid.ipnms.datamodel.router.RouterDesc;
->>>>>>> develop
 import tid.ipnms.datamodel.router.LabelSwitchedPath.LabelSwitchedPath;
 import tid.ipnms.datamodel.router.LabelSwitchedPath.LabelSwitchedPathProperties;
 import tid.ipnms.datamodel.router.LabelSwitchedPath.LabelSwitchedPathWithUnnumIf;
